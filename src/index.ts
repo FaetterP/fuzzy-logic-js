2639b5cb1bcb0e072e6acb1dd622ea1064fee2e3
FaetterP <56697273+FaetterP@users.noreply.github.com>
2023-09-16 12:22:28 +0300

    Публикация пакета в npm

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export * from "./fuzzySets/FuzzySet";
+export * from "./fuzzySets/Rectangle";
+export * from "./fuzzySets/Triangle";
+
+export * from "./operators/and";
+export * from "./operators/or";
+export * from "./operators/not";
+
+export * from "./operators/very";
+export * from "./operators/slightly";
+
+export * from "./operators/slice";
